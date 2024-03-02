02ee9e6238fde611cbbaee228fb93e3b33a3b166
John Doe <johndoe@example.com>
2024-03-02 12:11:11 +0300

    commit

--- lab5/zadanie1_lesson.py
+++ lab5/zadanie1_lesson.py
@@ -1,11 +1,17 @@
 import turtle
 turtle.shape("turtle")
-turtle.speed(5)
+turtle.speed(2)
 
 color = input("введите цвет")
 turtle.color(color)
 #каркас
-turtle.circle(50)
-turtle.right(50)
+turtle.circle(30)
+turtle.right(180)
 turtle.circle(40)
 
+turtle.penup()
+turtle.left(90)
+turtle.forward(80)
+turtle.pendown()
+turtle.right(90)
+turtle.circle(50)
