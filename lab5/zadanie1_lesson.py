d50fe8fcee547c07a7bf77feec4ff76fd84167ab
Grinch05 <maxdek57@gmail.com>
2024-03-15 18:10:22 +0300

    distance

--- lab5/zadanie1_lesson.py
+++ lab5/zadanie1_lesson.py
@@ -4,11 +4,10 @@ turtle.speed(5)
 
 color = input("введите цвет")
 turtle.color(color)
-#каркас
+#снеговик
 turtle.circle(30)
 turtle.right(180)
 turtle.circle(40)
-
 turtle.penup()
 turtle.left(90)
 turtle.forward(80)
--- lab7/zadanie1.py
+++ lab7/zadanie1.py
@@ -1,5 +1,5 @@
-i = 0
-while i != 6:
+i = 1
+while i != 5:
     print("*"*i)
     i=i+1
 print("Ёлочка")
